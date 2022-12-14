6897672b7bf817d7e4ab0065f1891e7e50956e49
Leila Daraei <107926155+LeilaDaraei@users.noreply.github.com>
2022-12-14 18:24:15 +0100

    added github repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        {" "}
+        This project was coded by Leila Daraei and is{" "}
+        <a
+          href="https://github.com/LeilaDaraei/react-weather-app"
+          alt="repository"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
