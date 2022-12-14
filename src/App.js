16e1ae0cf4e14db6730ff99bde2a62dd11c4ce85
Leila Daraei <107926155+LeilaDaraei@users.noreply.github.com>
2022-12-14 18:59:22 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        {" "}
-        This project was coded by Leila Daraei and is{" "}
-        <a
-          href="https://github.com/LeilaDaraei/react-weather-app"
-          alt="repository"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          {" "}
+          This project was coded by Leila Daraei and is{" "}
+          <a
+            href="https://github.com/LeilaDaraei/react-weather-app"
+            alt="repository"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
