b2cdbc2353196cec7935f68db13f89af2c398ffc
Leila Daraei <107926155+LeilaDaraei@users.noreply.github.com>
2022-12-15 17:52:56 +0100

    added forecast section

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="#ce9a69"
-      size={52}
+      size={props.size}
       animate={true}
     />
   );
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -16,7 +16,7 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div>
-            <WeatherIcon code={props.data.icon} />
+            <WeatherIcon code={props.data.icon} size={54} />
             <WeatherTemperature celsius={props.data.temperature} />
           </div>
         </div>
