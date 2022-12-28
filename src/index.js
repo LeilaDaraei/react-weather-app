e3ca64cd06bd0f21833528be9fe0b954790d2d5a
Leila Daraei <107926155+LeilaDaraei@users.noreply.github.com>
2022-12-28 11:13:22 +0100

    changed display

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div className="d-flex">
+    <div className="box">
       <Image className="d-block" />
       <App className="d-block" />
     </div>
